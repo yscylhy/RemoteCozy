039a7355d08e95d3a818b9149067e2446dfbf9c1
haoyi <lianghaoyi91@gmail.com>
2021-02-04 18:25:30 -0800

    webpage with the buzzer

--- arduino_usb_io.py
+++ arduino_usb_io.py
@@ -3,7 +3,7 @@ import serial
 
 
 class MyArduino:
-    commands = {"Turn off": "0", "Turn on": "1"}
+    commands = {"Turn off": "0", "Turn on": "1", "Buzzer on": 2, "Buzzer off": 3}
 
     def __init__(self, port=None, timeout=0.5):
         if port is None:
@@ -14,6 +14,14 @@ class MyArduino:
             else:
                 port = "/dev/ttyACM0"
         self.arduino = serial.Serial(port, 9600, timeout=timeout)
+
+    def turn_on(self):
+        self.send(self.commands["Buzzer on"])
+        self.send(self.commands["Turn on"])
+
+    def turn_off(self):
+        self.send(self.commands["Buzzer off"])
+        self.send(self.commands["Turn off"])
 
     def send(self, message):
         self.arduino.write("{}\n".format(message).encode())
--- web_app/routes.py
+++ web_app/routes.py
@@ -15,23 +15,32 @@ def index():
 
 @app.route('/ON')
 def turn_on():
-    arduino.send(arduino.commands["Turn on"])
+    arduino.turn_on()
     c_temp, f_temp = arduino.get_temp()
     return render_template('index.html', title='RemoteCozy', c_temp=c_temp, f_temp=f_temp)
 
 
 @app.route('/OFF')
 def turn_off():
-    arduino.send(arduino.commands["Turn off"])
+    arduino.turn_off()
+    c_temp, f_temp = arduino.get_temp()
+    return render_template('index.html', title='RemoteCozy', c_temp=c_temp, f_temp=f_temp)
+
+
+@app.route('/15MIN')
+def timer_15_min():
+    arduino.turn_on()
+    time.sleep(15*60)
+    arduino.turn_off()
     c_temp, f_temp = arduino.get_temp()
     return render_template('index.html', title='RemoteCozy', c_temp=c_temp, f_temp=f_temp)
 
 
 @app.route('/30MIN')
 def timer_30_min():
-    arduino.send(arduino.commands["Turn on"])
+    arduino.turn_on()
     time.sleep(30*60)
-    arduino.send(arduino.commands["Turn off"])
+    arduino.turn_off()
     c_temp, f_temp = arduino.get_temp()
     return render_template('index.html', title='RemoteCozy', c_temp=c_temp, f_temp=f_temp)
 
